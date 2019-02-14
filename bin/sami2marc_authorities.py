d8eb0a023b81747614c3dd9dbeef0998d7f146db
victoriamorris <victoria.morris@bl.uk>
2019-02-14 15:36:39 +0000

    Add files via upload

--- bin/sami2marc_authorities.py
+++ bin/sami2marc_authorities.py
@@ -28,6 +28,9 @@ __status__ = '4 - Beta Development'
 files = {
     'input':      None,
     'output':     None,
+    'pre':        None,
+    'post':       None,
+    'errors':     None,
     }
 
 roles = {
@@ -42,17 +45,32 @@ roles = {
 # ====================
 
 
-def usage():
+def usage(extended=False):
     """Function to print information about the program"""
     print('Correct syntax is:')
     print('sami2marc_authorities -i <ifile> -o <ofile>')
     print('    -i    path to Input file')
     print('    -o    path to Output file')
     print('\nUse quotation marks (") around arguments which contain spaces')
-    print('\nInput file should be SAMI Authorities file (.txt)')
+    print('\nInput file should be SAMI Authorities file (.txt or .prn)')
     print('\nOutput file should be either MARC exchange (.lex) or MARC XML (.xml)')
+    print('Records with errors will be written to <ofile>_errors.')
     print('\nOptions')
-    print('    --help    Display this message and exit')
+    print('    --date <yyyymmdd>')
+    print('              split output into two files by specfied date')
+    print('    --tidy    tidy authority files to facilitate load to MetAg')
+    print('    --help    Display help message and exit')
+    if extended:
+        print('\n\nIf parameter --date is specified:\n'
+              '\tRecords with a created or amended date ealier than \n'
+              '\tthe value specified will be written to <ofile>_pre_<date>; \n'
+              '\tRecords with a created or amended date equal to or later than \n'
+              '\tthe value specified will be written to <ofile>_post_<date>. \n')
+        print('\nIf parameter --tidy is specified:\n'
+              '\tIf no 001 is present, one will be created from the first instance of 901 $a; \n'
+              '\t904 $a and 905 $a will be combined into a single 904 field ($a and $b); \n'
+              '\t906 $a and 907 $a will be combined into a single 906 field ($a and $b); \n'
+              '\tCreated and Amended date will be converted from dd/mm/yy to yyyymmdd format. \n')
     exit_prompt()
 
 
@@ -64,79 +82,138 @@ def usage():
 def main(argv=None):
     if argv is None: name = str(sys.argv[1])
 
-    xml = False
-    opts, args = None, None
+    xml, tidy = False, False
+    opts, args, date = None, None, None
 
     print('========================================')
     print('sami2marc_authorities')
     print('========================================')
     print('\nThis program converts SAMI AUTHORITY files\n'
-          'from text format\n'
+          'from text or .prn format\n'
           'to MARC 21 Authority files in MARC exchange (.lex) or MARC XML format\n')
 
-    try: opts, args = getopt.getopt(argv, 'i:o:', ['ifile=', 'ofile=', 'help'])
+    try: opts, args = getopt.getopt(argv, 'i:o:d:', ['ifile=', 'ofile=', 'date=', 'tidy', 'help'])
     except getopt.GetoptError as err:
         exit_prompt('Error: {}'.format(err))
     if opts is None or not opts:
         usage()
     for opt, arg in opts:
         if opt == '--help':
-            usage()
+            usage(extended=True)
+        elif opt == '--tidy':
+            tidy = True
+        elif opt in ['-d', '--date']:
+            date = arg
         elif opt in roles:
             files[roles[opt]] = FilePath(arg, roles[opt])
         else:
             exit_prompt('Error: Option {} not recognised'.format(opt))
 
-    for f in files:
+    for f in ['input', 'output']:
         if not files[f]:
             exit_prompt('Error: No path to {} file has been specified'.format(f))
     if files['output'].ext == '.xml': xml = True
 
+    # Create extra files for MetAg output
+    if date:
+        if len(re.sub(r'[^0-9]]', '', date)) != 8: exit_prompt('The date parameter must be in the format yyyymmdd')
+        try: date = datetime.datetime.strptime(date, '%Y%m%d')
+        except: exit_prompt('The date parameter must be in the format yyyymmdd')
+        files['pre'] = FilePath((files['output'].path).replace(files['output'].ext,
+                                                               '_pre_{}{}'.format(date.strftime('%Y%m%d'), files['output'].ext)),
+                                                               'output pre date')
+        files['post'] = FilePath((files['output'].path).replace(files['output'].ext,
+                                                                '_post_{}{}'.format(date.strftime('%Y%m%d'), files['output'].ext)),
+                                                                'output post date')
+
+    files['errors'] = FilePath((files['output']).path.replace(files['output'].ext, '_errors{}'.format(files['output'].ext)), 'error output')
+
     print('\n\nStarting conversion ...')
     print('----------------------------------------')
     print(str(datetime.datetime.now()))
+    if date:
+        print('\nDate for splitting output: {}'.format(date.strftime('%Y%m%d')))
+    if date or tidy: print('Output will be tidied for MetAg use.\n')
 
     ifile = open(files['input'].path, mode='r', encoding='utf-8', errors='replace')
-    if xml:
-        ofile = open(files['output'].path, mode='w', encoding='utf-8', errors='replace')
-        ofile.write('<?xml version="1.0" encoding="UTF-8" ?>'
-                    '\n<marc:collection xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
-                    'xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">')
-    else:
-        ofile = open(files['output'].path, mode='wb')
-        writer = MARCWriter(ofile)
+    for f in files:
+        if f != 'input' and files[f]:
+            if xml:
+                files[f].file_object = open(files[f].path, mode='w', encoding='utf-8', errors='replace')
+                files[f].file_object.write(XML_HEADER)
+            else:
+                files[f].file_object = open(files[f].path, mode='wb')
+                files[f].file_writer = MARCWriter(files[f].file_object)
 
     record_count = 0
-    in_record, in_field = False, False
+    in_record, in_field, error = False, False, False
     field_content = ''
+    sid, fmt, level, created, created_by, modified, modified_by, cataloged, source = None, None, None, None, None, None, None, None, None
     for filelineno, line in enumerate(ifile):
         line = line.strip('\n')
         if line.startswith('XX'):
             record_count += 1
-            print('\r{0} SAMI records processed'.format(str(record_count)), end='\r')
+            if record_count % 100 == 0:
+                print('\r{0} SAMI records processed'.format(str(record_count)), end='\r')
+
+            # Write current record
             if in_record:
                 if in_field:
                     record.add_ordered_field(line_to_field(field_content))
-                if xml:
-                    ofile.write(record.as_xml())
-                else:
-                    writer.write(record)
+                if tidy:
+                    if '001' not in record:
+                        if sid != '':
+                            record.add_ordered_field(Field(tag='001', data=sid))
+                        else:
+                            print('Failed to add 001')
+                            error = True
 
+                if error:
+                    if xml: files['errors'].file_object.write(record.as_xml())
+                    else: files['errors'].file_writer.write(record)
+                else:
+                    if xml: files['output'].file_object.write(record.as_xml())
+                    else: files['output'].file_writer.write(record)
+                    if date:
+                        fmt = '%Y%m%d' if tidy else '%d/%m/%Y'
+                        try:
+                            f = 'post' if (created != 'NEVER' and datetime.datetime.strptime(created, fmt) >= date) \
+                                          or (modified != 'NEVER' and datetime.datetime.strptime(modified, fmt) >= date) else 'pre'
+                        except:
+                            print('\nError parsing date')
+                        else:
+                            if xml: files[f].file_object.write(record.as_xml())
+                            else: files[f].file_writer.write(record)
+
+            # Start new record
             record = MARCRecord()
-            in_record, in_field = True, False
+            in_record, in_field, error = True, False, False
             field_content = ''
-            sid, fmt, level, created, created_by, modified, modified_by, cataloged, source = line.rstrip('\t').split(
-                '\t\t')
+            sid, fmt, level, created, created_by, modified, modified_by, cataloged, source = line.rstrip('\t').split('\t\t')
+            if tidy:
+                if created != 'NEVER':
+                    try: created = datetime.datetime.strftime(datetime.datetime.strptime(created, '%d/%m/%Y'), '%Y%m%d')
+                    except:
+                        print('Error parsing created date')
+                        error = True
+                if modified != 'NEVER':
+                    try: modified = datetime.datetime.strftime(datetime.datetime.strptime(modified, '%d/%m/%Y'), '%Y%m%d')
+                    except:
+                        print('Error parsing modified date')
+                        error = True
             record.add_field(Field(tag='901', indicators=[' ', ' '], subfields=['a', 'id: ' + sid]))
             record.add_field(Field(tag='902', indicators=[' ', ' '], subfields=['a', 'fmt: ' + fmt]))
             record.add_field(Field(tag='903', indicators=[' ', ' '], subfields=['a', 'level: ' + level]))
-            record.add_field(Field(tag='904', indicators=[' ', ' '], subfields=['a', 'created: ' + created]))
-            record.add_field(Field(tag='905', indicators=[' ', ' '], subfields=['a', 'created_by: ' + created_by]))
-            record.add_field(Field(tag='906', indicators=[' ', ' '], subfields=['a', 'modified: ' + modified]))
-            record.add_field(Field(tag='907', indicators=[' ', ' '], subfields=['a', 'modified_by: ' + modified_by]))
+            if tidy:
+                record.add_field(Field(tag='904', indicators=[' ', ' '], subfields=['a', 'created: ' + created, 'b', 'created_by: ' + created_by]))
+                record.add_field(Field(tag='906', indicators=[' ', ' '], subfields=['a', 'modified: ' + modified, 'b', 'modified_by: ' + modified_by]))
+            else:
+                record.add_field(Field(tag='904', indicators=[' ', ' '], subfields=['a', 'created: ' + created]))
+                record.add_field(Field(tag='905', indicators=[' ', ' '], subfields=['a', 'created_by: ' + created_by]))
+                record.add_field(Field(tag='906', indicators=[' ', ' '], subfields=['a', 'modified: ' + modified]))
+                record.add_field(Field(tag='907', indicators=[' ', ' '], subfields=['a', 'modified_by: ' + modified_by]))
             record.add_field(Field(tag='908', indicators=[' ', ' '], subfields=['a', 'cataloged: ' + cataloged]))
             record.add_field(Field(tag='909', indicators=[' ', ' '], subfields=['a', 'source: ' + source]))
-
 
         elif in_record:
             line = line.strip()
@@ -149,18 +226,44 @@ def main(argv=None):
             else:
                 field_content += line
 
+    # Write final record
     if in_record:
         if in_field:
             record.add_ordered_field(line_to_field(field_content))
-        if xml:
-            ofile.write(record.as_xml())
+        if '001' not in record:
+            if sid != '':
+                record.add_ordered_field(Field(tag='001', data=sid))
+            else: error = True
+        if error:
+            if xml: files['errors'].file_object.write(record.as_xml())
+            else: files['errors'].file_writer.write(record)
         else:
-            writer.write(record)
+            if xml: files['output'].file_object.write(record.as_xml())
+            else: files['output'].file_writer.write(record)
+            if date:
+                try:
+                    fmt = '%Y%m%d' if tidy else '%d/%m/%Y'
+                    f = 'post' if (created != 'NEVER' and datetime.datetime.strptime(created, fmt) >= date) \
+                                  or (modified != 'NEVER' and datetime.datetime.strptime(modified, fmt) >= date) else 'pre'
+                except Exception as e:
+                    print('\nError parsing date')
+                    print(str(e))
+                    print(created)
+                    print(modified)
+                else:
+                    if xml: files[f].file_object.write(record.as_xml())
+                    else: files[f].file_writer.write(record)
 
-    if xml: ofile.write('\n</marc:collection>')
+    # Close XML record
+    if xml:
+        for f in files:
+            if f != 'input' and files[f]:
+                files[f].file_object.write('\n</marc:collection>')
+
     # Close files
-    for f in (ifile, ofile):
-        f.close()
+    for f in files:
+        try: files[f].file_object.close()
+        except: pass
 
     date_time_exit()
 
--- bin/sami2marc_products.py
+++ bin/sami2marc_products.py
@@ -6,6 +6,7 @@
 # ====================
 
 # Import required modules
+from math import log10
 from samiTools.marc_data import *
 
 # Set locale to assist with sorting
@@ -25,7 +26,7 @@ __status__ = '4 - Beta Development'
 # ====================
 
 
-def usage():
+def usage(extended=False):
     """Function to print information about the program"""
     print('Correct syntax is:')
     print('sami2marc_products -i <input_folder> -o <output_folder>')
@@ -35,7 +36,25 @@ def usage():
     print('\nInput files should be SAMI files, in .xml, .prn or text format')
     print('\nOptions')
     print('    -x        Output files will be MARC XML rather than MARC 21 (.lex)')
-    print('    --help    Display this message and exit')
+    print('    --max_size <number|size>')
+    print('              Split output')
+    print('    --help    Display help message and exit')
+    if extended:
+        print('\n\nIf parameter --max_size is specified:\n'
+              '\tmax_size is EITHER the maximum number of records in an output file \n'
+              '\tOR the (approx.) maximum file size (in KB) \n'
+              '\tif the number has the suffix \'K\'; \n\n'
+              '\tOutput will be written to a sequence of files with the same name \n'
+              '\tas the input file, but with a suffix indicating its order in the \n'
+              '\tgenerated output sequence \n\n'
+              '\tEXCEPT in the special case of --max_size 1 \n'
+              '\t(the file is split into individual records) \n'
+              '\tin which case the output files will be labelled \n'
+              '\twith the record identifier.\n '
+              '\tRecords with duplicate identifiers will be labelled \n'
+              '\twith a _DUPLICATE suffix.'
+              '\tRecords without identifiers will be labelled \n'
+              '\twith _NO IDENTIFIER.')
     exit_prompt()
 
 
@@ -47,32 +66,45 @@ def usage():
 def main(argv=None):
     if argv is None: name = str(sys.argv[1])
 
-    xml = False
+    xml, split = False, False
     opts, args = None, None
     input_path, output_path = None, None
+    limit = None
+    max_size = 1024 * 1024 * 1024
 
     print('========================================')
     print('sami2marc_products')
     print('========================================')
     print('\nThis program converts SAMI files\n'
-          'from text or XML format\n'
+          'from text or .prn or XML format\n'
           'to MARC 21 Bibliographic files in MARC exchange (.lex) or MARC XML format\n')
 
     try:
-        opts, args = getopt.getopt(argv, 'i:o:', ['input_path=', 'output_path=', 'help'])
+        opts, args = getopt.getopt(argv, 'i:o:m:x', ['input_path=', 'output_path=', 'max_size=', 'help'])
     except getopt.GetoptError as err:
         exit_prompt('Error: {}'.format(err))
     if opts is None or not opts:
         usage()
     for opt, arg in opts:
         if opt == '--help':
-            usage()
+            usage(extended=True)
         elif opt == '-x':
             xml = True
         elif opt in ['-i', '--input_path']:
             input_path = arg
         elif opt in ['-o', '--output_path']:
             output_path = arg
+        elif opt in ['-m', '--max_size']:
+            arg = arg.upper()
+            limit = 'size' if 'K' in arg else 'number'
+            try: max_size = int(re.sub(r'[^0-9]', '', arg))
+            except: exit_prompt('Maximum file size could not be interpreted. \n'
+                                'Please ensure that it is a positive integer, \n'
+                                'optionally followed by the suffix K.')
+            if not max_size >= 1: exit_prompt('Maximum file size could not be interpreted. \n'
+                                              'Please ensure that it is a positive integer, \n'
+                                              'optionally followed by the suffix K.')
+            if limit == 'size': max_size *= 1024
         else:
             exit_prompt('Error: Option {} not recognised'.format(opt))
 
@@ -83,7 +115,8 @@ def main(argv=None):
     if not output_path:
         exit_prompt('Error: No path to output files has been specified')
     if not os.path.isdir(output_path):
-        exit_prompt('Error: Invalid path to output files')
+        try: os.makedirs(output_path)
+        except: exit_prompt('Error: Could not parse path to output files')
 
     # --------------------
     # Parameters seem OK => start program
@@ -93,47 +126,107 @@ def main(argv=None):
 
     print('Input folder: {}'.format(input_path))
     print('Output folder: {}'.format(output_path))
+    if limit:
+        if max_size == 1:
+            split = True
+            print('Output file will be split into individual records')
+        else: print('Maximum file size : {} {}'.format(str(max_size), 'bytes' if limit == 'size' else 'records'))
 
     # --------------------
     # Iterate through input files
     # --------------------
 
     for file in os.listdir(input_path):
-        if file.endswith(('.xml', '.prn')) or file.endswith(SAMI_SUFFICES):
+        root, ext = os.path.splitext(file)
+        if ext in ['.xml', '.prn'] or file.endswith(SAMI_SUFFICES):
 
             date_time('Processing file {} ...'.format(str(file)))
 
-            # Open input and output files
+            # Open input file
             ifile = open(os.path.join(input_path, file), mode='r', encoding='utf-8', errors='replace')
-            reader_type = 'prn' if file.endswith('.prn') else 'xml' if file.endswith('.xml') else 'txt'
+            reader_type = 'prn' if ext == '.prn' else 'xml' if ext == '.xml' else 'txt'
+            ext = '.xml' if xml else '.lex'
             reader = SAMIReader(ifile, reader_type=reader_type)
-            tfile = open(os.path.join(output_path, file + '.txt'), mode='w', encoding='utf-8', errors='replace')
+
+            # Special case if file is to be split into separate records
+            if split:
+                record_count = 0
+                for record in reader:
+                    record_count += 1
+                    if record_count % 100 == 0:
+                        print('{} records processed'.format(str(record_count)), end='\r')
+                    filename = os.path.join(output_path, (record.identifier() or '_NO IDENTIFIER {}'.format(str(record_count))) + ext)
+                    file_count = 0
+                    while os.path.isfile(filename):
+                        file_count += 1
+                        filename = os.path.join(output_path, (record.identifier() or '_NO IDENTIFIER {}'.format(str(record_count))) + '_DUPLICATE {}'.format(str(file_count)) + ext)
+                    if xml:
+                        current_file = open(filename, 'w', encoding='utf-8', errors='replace')
+                        current_file.write(XML_HEADER)
+                        current_file.write(record.as_xml())
+                        current_file.write('\n</marc:collection>')
+                    else:
+                        current_file = open(filename, mode='wb')
+                        writer = MARCWriter(current_file)
+                        writer.write(record)
+                    current_file.close()
+                ifile.close()
+                print('{} records processed'.format(str(record_count)), end='\r')
+                continue
+
+            # All other cases
+            FMT = None
+            record_count, record_count_in_file = 0, 0
+            current_idx, current_size = 0, 0
+
+            if limit == 'size':
+                FMT = ".%%0%dd" % (int(log10(os.path.getsize(os.path.join(input_path, file)) / max_size)) + 1)
+
+            mid = FMT % current_idx if limit == 'size' else '.{}'.format(str(current_idx)) if limit == 'number' else ''
+            filename = os.path.join(output_path, root + mid + ext)
+
             if xml:
-                ofile = open(os.path.join(output_path, file + '.xml'), mode='w', encoding='utf-8', errors='replace')
-                ofile.write('<?xml version="1.0" encoding="UTF-8" ?>'
-                            '\n<marc:collection xmlns:marc="http://www.loc.gov/MARC21/slim" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '
-                            'xsi:schemaLocation="http://www.loc.gov/MARC21/slim http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">')
+                current_file = open(filename, 'w', encoding='utf-8', errors='replace')
+                current_file.write(XML_HEADER)
             else:
-                ofile = open(os.path.join(output_path, file + '.lex'), mode='wb')
-                writer = MARCWriter(ofile)
-            i = 0
+                current_file = open(filename, mode='wb')
+                writer = MARCWriter(current_file)
 
             for record in reader:
-                i += 1
-                print('{} records processed'.format(str(i)), end='\r')
-                if xml:
-                    ofile.write(record.as_xml())
-                else:
-                    writer.write(record)
-                tfile.write(str(record) + '\n')
+                record_count += 1
+                record_count_in_file += 1
+                if record_count % 100 == 0:
+                    print('{} records processed'.format(str(record_count)), end='\r')
 
-            if xml: ofile.write('\n</marc:collection>')
+                # Check whether we need to start a new file
+                current_size += len(record.as_xml()) if xml else len(record.as_marc())
+                if ( limit == 'size' and current_size >= max_size ) \
+                        or ( limit == 'number' and record_count_in_file > max_size ):
+                    if xml: current_file.write('\n</marc:collection>')
+                    current_file.close()
+                    print('\nFile {} done'.format(str(current_idx)))
+                    current_size = len(record.as_xml()) if xml else len(record.as_marc())
+                    record_count_in_file = 0
+                    current_idx += 1
+                    mid = FMT % current_idx if limit == 'size' else '.{}'.format(str(current_idx)) if limit == 'number' else ''
+                    filename = os.path.join(output_path, root + mid + ext)
+                    if xml:
+                        current_file = open(filename, 'w', encoding='utf-8', errors='replace')
+                        current_file.write(XML_HEADER)
+                    else:
+                        current_file = open(filename, mode='wb')
+                        writer = MARCWriter(current_file)
+
+                if xml: current_file.write(record.as_xml())
+                else: writer.write(record)
+
+            if xml: current_file.write('\n</marc:collection>')
+            print('{} records processed'.format(str(record_count)), end='\r')
             # Close files
-            for f in (ifile, ofile, tfile):
+            for f in [ifile, current_file]:
                 f.close()
 
     date_time_exit()
-
 
 if __name__ == '__main__':
     main(sys.argv[1:])
